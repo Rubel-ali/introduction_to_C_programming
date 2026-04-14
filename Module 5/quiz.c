751bfd6e697a7e027c9a7d8d5ea90f9091d2e361
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2026-04-14 09:11:26 +0600

    Max and Min problem solve

---
+++ Module 5/quiz.c
@@ -0,0 +1,64 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int x =4;
+//     scanf("%d", x);
+//     if (x % 2 != 0)
+//     {
+//         printf("Odd");
+//     }
+//     else
+//     {
+//         printf("Event");
+//     }
+//     if (x > 0)
+//     {
+//         printf("positive");
+//     }
+//     else
+//     {
+//         printf("Negative");
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int x = 0;
+//     if (x == 1)
+//     {
+//         if (x == 0)
+//         {
+//             printf("inside if if\n");
+//         }
+//         else
+//         {
+//             printf("inside if else\n");
+//         }
+//     }
+//     else
+//     {
+//         printf("inside else\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int marks = 76;
+    if (marks >= 80)
+    {
+        printf("A+");
+    }
+    else if (marks >= 70)
+    {
+        printf("A");
+    }
+    else if (marks >= 70)
+    {
+        printf("A-");
+    }
+    return 0;
+}
